734bc02c61d262f48743054539d4fac4d492ff98
pieter melis <pieter.melis@hotmail.com>
2017-09-30 14:57:20 +0200

    add wedstrijd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <img src="{{ asset('img/logo.png') }}" alt="">
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url('/wedstrijd') }}">Wedstrijd</a>
+                    <a href="{{ url('/winnaars') }}">Winnaars</a>
+                    <a href="http://www.volvo.com/home.html">Over Volvo</a>
                 </div>
             </div>
         </div>
